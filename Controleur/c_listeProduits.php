12b7d4ae4d24678d710c88f7222a7238ce7423a7
guyonnet nautic <quentin.guyonnet@greta-sud-aquitaine.academy>
2018-02-05 09:32:17 +0100

    c_listeFournisseur,c_Produit & insertions

--- Controleur/c_listeProduits.php
+++ Controleur/c_listeProduits.php
@@ -1,1 +1,48 @@
 <?php
+
+$action = "";
+if(!empty($_REQUEST['a'])){
+    $action = $_REQUEST['a'];
+}
+switch($action)
+{
+    case "afficher": { // a changer selon besoin
+        include('Vue/backend/v_listeProduits.php');
+        break;
+    }
+    case "nouveau":{
+        include('Vue/backend/v_pageProduit.php');
+        break;
+    }
+    case "ajout": // a changer selon besoin
+        var_dump($_REQUEST);
+        if(!empty($_POST['nomArticle']) && !empty($_POST['referenceArticle'])){
+            $nom = $_POST['nomArticle'];
+            $reference = $_POST['referenceArticle'];
+            $prix = $_POST['prixTTCArticle'];
+            $resume = $_POST['resumeArticle'];
+            $descr = $_POST['descriptionArticle'];
+            $qte = $_POST['quantiteArticle'];
+            $poids = $_POST['poidsArticle'];
+            $motor = $_POST['motorisationArticle'];
+            $dimension = $_POST['longueurArticle'];
+            $photo = $_POST['photoArticle'];
+
+            if ($nom != ''){
+                nouveauArticle($bdd,$reference,$nom,$prix,$resume,$descr,$qte,$poids,$motor,$dimension,$photo);
+                $validation = "L'article a bien été ajouté";
+                var_dump($validation);
+            }else{
+                $erreur = "Le nom de l'article ne peut être vide";
+            }
+        }else{
+            $erreur = "Le nom de l'article ne peux être vide";
+        }
+        include('Vue/backend/v_pageProduit.php');
+        break;
+    default:
+        include("Vue/backend/v_accueil.php");
+        break;
+}
+
+?>
--- Modele/m_insertions.php
+++ Modele/m_insertions.php
@@ -3,27 +3,47 @@
 function nouveauCategorie($bdd,$nom, $descri){
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $stmt = $bdd->prepare("INSERT INTO categorie (nom_categorie,desc_categorie)VALUES(:nom,:descri)");
-    $stmt->bindParam(':nom', $nom);
-    $stmt->bindParam(':descri', $descri);
-    $stmt->execute();
+
+    $params = array(
+        ':nom' => $nom,
+        ':descri' => $descri,
+    );
+
+    $stmt->execute($params);
 }
 
-function nouveauArticle($bdd,$ref,$nom,$prix,$resume,$descr,$qte,$poids,$motor,$dimension,$photo,$tva,$categorie,$marque)
+function nouvelleMarque($bdd,$nom,$logo){
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $bdd->prepare("INSERT INTO marque (nom_marque,logo_marque) VALUES (:nom,:logo)");
+
+    $params = array (
+        ':nom' => $nom,
+        ':logo' => $logo,
+    );
+
+    $stmt->execute($params);
+}
+
+
+function nouveauArticle($bdd,$ref,$nom,$prix,$resume,$descr,$qte,$poids,$motor,$dimension,$photo,$marque,$tva)
 {
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = "INSERT INTO article (reference,nom_article,prix_article,qte_article,resume_article,desc_article,poids_article,motorisation_article,dimensions_article,photo_article) VALUES (:ref,:nom,:prix,:qte,:resume,:descr,:poids,:motor,:dimension,:photo,:marque,:tva)";
-    $stmt->bindParam(':ref', $ref);
-    $stmt->bindParam(':nom', $nom);
-    $stmt->bindParam(':prix', $prix);
-    $stmt->bindParam(':qte', $qte);
-    $stmt->bindParam(':resume', $resume);
-    $stmt->bindParam(':descr', $descr);
-    $stmt->bindParam(':poids', $poids);
-    $stmt->bindParam(':motor', $motor);
-    $stmt->bindParam(':dimension', $dimension);
-    $stmt->bindParam(':photo', $photo);
-    $stmt->bindParam(':marque', $marque);
-    $stmt->bindParam(':tva', $tva);
+    $stmt = $bdd->prepare("INSERT INTO article (reference,nom_article,prix_article,qte_article,resume_article,desc_article,poids_article,motorisation_article,dimensions_article,photo_article,id_marque,id_tva) VALUES (:ref,:nom,:prix,:qte,:resume,:descr,:poids,:motor,:dimension,:photo,:marque,:tva)");
 
-    $bdd->exec($stmt);
+    $params = array (
+        ':ref' => $ref,
+        ':nom' => $nom,
+        ':prix' => $prix,
+        ':qte' => $qte,
+        ':resume' => $resume,
+        ':descr' => $descr,
+        ':poids' => $poids,
+        ':motor' => $motor,
+        ':dimension' => $dimension,
+        ':photo' => $photo,
+        ':marque' => $marque,
+        ':tva' => $tva,
+    );
+
+    $stmt->execute($params);
 }
